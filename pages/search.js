0b236b02c291002f0e6507d3252e4007e38ac3d5
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-03 14:20:14 -0500

    working search bar

--- pages/search.js
+++ pages/search.js
@@ -1,27 +1,11 @@
-import axios from 'axios';
 import Header from '../src/components/Header/Header';
 
 const Search = () => {
-  const searchForHalo = () => {
-    axios({
-      url: 'https://peaceful-tor-54406.herokuapp.com/https://api.igdb.com/v4/games',
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Client-ID': process.env.IGDB_CLIENT_ID,
-        Authorization: process.env.IGDB_AUTH_TOKEN,
-      },
-      data: 'fields name',
-    })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  };
-
   return (
     <div>
       <Header />
       <h1>This is the search page</h1>
-      <button onClick={searchForHalo}>SEARCH</button>
+      {/* <button onClick={searchForGame}>SEARCH</button> */}
     </div>
   );
 };
--- src/components/Header/Searchbar.js
+++ src/components/Header/Searchbar.js
@@ -1,4 +1,32 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 const Searchbar = () => {
+  const [search, setSearch] = useState('');
+
+  const showSearch = (e) => {
+    setSearch(e.target.value);
+
+    console.log(search);
+  };
+
+  const searchForGame = (event) => {
+    event.preventDefault();
+
+    axios({
+      url: `https://peaceful-tor-54406.herokuapp.com/https://api.igdb.com/v4/games/?search=${search}&fields=id,name,summary`,
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Client-ID': process.env.IGDB_CLIENT_ID,
+        Authorization: process.env.IGDB_AUTH_TOKEN,
+      },
+      data: 'fields name;',
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <form className="d-flex">
       <input
@@ -6,8 +34,13 @@ const Searchbar = () => {
         type="search"
         placeholder="Search"
         aria-label="Search"
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <button className="btn btn-outline-success" type="submit">
+      <button
+        className="btn btn-outline-success"
+        type="submit"
+        onClick={searchForGame}
+      >
         Search
       </button>
     </form>
