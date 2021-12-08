76a2c11aef80f39c18fffac82a07be1dea61dd3f
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-08 12:02:38 -0500

    Added new axios request for trending releases

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Game from './Game';
+import axios from 'axios';
 
 const Trending = () => {
+  let [trendingGames, setTrendingGames] = useState();
+
+  useEffect(() => {
+    const config = {
+      method: 'get',
+      url: 'https://api.twitch.tv/helix/games/top?limit=6',
+      headers: {
+        accept: 'application/json',
+        'Client-ID': 'dmlu2pmbd2wc14ke1ooh3on7cmnxr9',
+        Authorization: 'Bearer yrehr7e8zvm73i51nafp0w0c8d74ra',
+      },
+    };
+
+    const getGames = async () => {
+      await axios(config)
+        .then((res) => {
+          setTrendingGames(res.data);
+          console.log(trendingGames);
+        })
+        .catch((err) => console.log(err));
+    };
+
+    getGames();
+  }, []);
+
   return (
     <div className="bg-black text-white">
       <div className="container">
         <h1>Trending Releases</h1>
         <div className="row">
-          <Game
-            name={'Halo 3'}
-            img={
-              'https://www.dualshockers.com/static/uploads/2017/09/halo-3.jpg'
-            }
-          />
-          <Game
-            name={'Metal Gear'}
-            img={
-              'https://www.dualshockers.com/static/uploads/2017/09/halo-3.jpg'
-            }
-          />
-          <Game
-            name={'Silent Hill'}
-            img={
-              'https://www.dualshockers.com/static/uploads/2017/09/halo-3.jpg'
-            }
-          />
+          {trendingGames &&
+            trendingGames.data.slice(1, 4).map((game) => {
+              return (
+                <Game key={game.id} name={game.name} img={game.box_art_url} />
+              );
+            })}
         </div>
         <div className="row">
           <Game
